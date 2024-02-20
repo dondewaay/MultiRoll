e03119ec957d0cc3bf131dcc516b3259d8bdd769
ddewaay <dondewaay@gmail.com>
2024-02-19 20:17:44 -0600

    More logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,37 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-const Card = ({ name, config }) => {
+const Card = ({ key, name, config }) => {
   const [totalRoll, setTotalRoll] = useState(0);
 
   const calculateTotalRoll = () => {
     let total = 0;
-    for (let i = 0; i <config.die; i++) {
-
-      total += Math.floor(Math.random() * config[die] + 1);
+    for (i = 0; i < config.d2; i++) {
+      total += total += Math.floor(Math.random() * 2 + 1);
+    }
+    for (i = 0; i < config.d3; i++) {
+      total += total += Math.floor(Math.random() * 3 + 1);
+    }
+    for (i = 0; i < config.d4; i++) {
+      total += total += Math.floor(Math.random() * 4 + 1);
+    }
+    for (i = 0; i < config.d6; i++) {
+      total += total += Math.floor(Math.random() * 6 + 1);
+    }
+    for (i = 0; i < config.d8; i++) {
+      total += total += Math.floor(Math.random() * 8 + 1);
+    }
+    for (i = 0; i < config.d10; i++) {
+      total += total += Math.floor(Math.random() * 10 + 1);
+    }
+    for (i = 0; i < config.d12; i++) {
+      total += total += Math.floor(Math.random() * 12 + 1);
+    }
+    for (i = 0; i < config.d20; i++) {
+      total += total += Math.floor(Math.random() * 20 + 1);
+    }
+    for (i = 0; i < config.d100; i++) {
+      total += total += Math.floor(Math.random() * 100 + 1);
     }
     setTotalRoll(total);
   };
@@ -21,6 +44,7 @@ const Card = ({ name, config }) => {
       transition={{ duration: 0.3 }}
     >
       <h2>{name}</h2>
+      <h2>{key}</h2>
       <p>Total Roll: {totalRoll}</p>
       <button className="btn" onClick={calculateTotalRoll}>
         Roll
