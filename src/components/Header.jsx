6daa4e354dc9b65b8bb397c215b890842a682423
DonDeWaay <dondewaay@gmail.com>
2024-02-27 04:49:57 -0600

    More design fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
           className="menu"
           initial={{ y: -50 }}
           animate={{ y: 0 }}
-          transition={{ type: "spring", duration: 0.3 }}
+          transition={{ type: "spring", duration: 1 }}
           whileHover={{
             scale: 1.07,
           }}
@@ -28,7 +28,7 @@ export default function Header() {
           className="menu"
           initial={{ y: -50 }}
           animate={{ y: 0 }}
-          transition={{ duration: 0.3 }}
+          transition={{ duration: 1 }}
           whileHover={{
             delay: 0,
             scale: 1.25,
