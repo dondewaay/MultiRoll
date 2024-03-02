9acc73d8c0b490f6f35a779fcbf91ddc596b9c87
DonDeWaay <dondewaay@gmail.com>
2024-03-01 18:16:21 -0600

    Scrolling element oopsie

--- src/components/ScrollingText.jsx
+++ src/components/ScrollingText.jsx
@@ -1,35 +1,37 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ScrollingText = ({ content, colorSel }) => {
   const divStyle = {
     color: colorSel,
-    WebkitTextStrokeColor: "#e43737",
+    WebkitTextStrokeColor: colorSel,
   };
 
   return (
     <div style={divStyle}>
-      <motion.div
-        className="scroll-text"
-        animate={{ x: -100000 }}
-        transition={{ duration: 800, ease: "linear", repeat: Infinity }}
-        while={{ x: 0 }}
-      >
-        {content}&nbsp;
-        <span>{content}&nbsp; </span>
-        {content}&nbsp;
-        <span>{content}&nbsp; </span>
-        {content}&nbsp;
-        <span>{content}&nbsp; </span>
-        {content}&nbsp;
-        <span>{content}&nbsp; </span>
-        {content}&nbsp;
-        <span>{content}&nbsp; </span>
-        {content}&nbsp;
-        <span>{content}&nbsp; </span>
-        {content}&nbsp;
-        <span>{content}&nbsp; </span>
-        {content}&nbsp;
-      </motion.div>
+      <AnimatePresence>
+        <motion.div
+          className="scroll-text"
+          animate={{ x: -100000 }}
+          transition={{ duration: 800, ease: "linear", repeat: Infinity }}
+          while={{ x: 0 }}
+        >
+          {content}&nbsp;
+          <span>{content}&nbsp; </span>
+          {content}&nbsp;
+          <span>{content}&nbsp; </span>
+          {content}&nbsp;
+          <span>{content}&nbsp; </span>
+          {content}&nbsp;
+          <span>{content}&nbsp; </span>
+          {content}&nbsp;
+          <span>{content}&nbsp; </span>
+          {content}&nbsp;
+          <span>{content}&nbsp; </span>
+          {content}&nbsp;
+          <span>{content}&nbsp; </span>
+          {content}&nbsp;
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
